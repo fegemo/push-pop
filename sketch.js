a3a7e87cdd08d956bc018ea1cc0aff686beef6c8
Flávio Coutinho <fegemo@gmail.com>
2018-09-06 16:45:46 -0300

    Improves the code quality and adds few more buttons

--- sketch.js
+++ sketch.js
@@ -9,7 +9,117 @@ var tam = 1;
 var mulTam = 0.001;
 var canvas;
 
+class Button {
+	constructor(text, x, y, width, height) {
+		this.text = text;
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+	}
 
+	draw() {
+		stroke(0, 0, 0);
+		rect(this.x, this.y, this.width, this.height);
+		textSize(15);
+		text(this.text, this.x + 5, this.y + 20);
+	}
+
+	isPointWithin(x, y) {
+		return this.x <= x && this.x + this.width >= x && this.y <= y && this.y + this.height >= y;
+	}
+
+	execute() {
+		alert('Ainda não implementado');
+	}
+}
+
+class PushMatrixButton extends Button {
+	constructor(x, y, width, height) {
+		super('glPushMatrix( )', x, y, width, height);
+	}
+
+	execute() {
+		if (Matrizes.length == 0) {
+			var d3 = [];
+			arrayCopy(id2, d3);
+			Matrizes.push(new Matrix(d3, height - 51));
+		} else if (Matrizes.length == 1) {
+			if (Matrizes[Matrizes.length - 1].bot) {
+				var d4 = [];
+				arrayCopy(Matrizes[Matrizes.length - 1].numbers, d4);
+				Matrizes.push(new Matrix(d4, ((Matrizes[Matrizes.length - 1]).pos.y) - 80));
+			}
+		} else if (Matrizes.length < 4) {
+			if (Matrizes[Matrizes.length - 1].bot) {
+				var d5 = [];
+				arrayCopy(Matrizes[Matrizes.length - 1].numbers, d5);
+				Matrizes.push(new Matrix(d5, ((Matrizes[Matrizes.length - 1]).pos.y) - 80));
+			}
+		}
+	}
+}
+
+class PopMatrixButton extends Button {
+	constructor(x, y, width, height) {
+		super('glPopMatrix( )', x, y, width, height);
+	}
+
+	execute() {
+		Matrizes.splice(Matrizes.length - 1, 1);
+	}
+}
+
+class LoadMatrixButton extends Button {
+	constructor(x, y, width, height) {
+		super('glLoadMatrix(...)', x, y, width, height);
+	}
+
+	execute() {
+		if (Matrizes.length > 0) {
+			insertedMatrix(Matrizes[Matrizes.length - 1].numbers);
+		}
+	}
+}
+
+class MultMatrixButton extends Button {
+	constructor(x, y, width, height) {
+		super('glMultMatrix(...)', x, y, width, height);
+	}
+
+	execute() {
+		if (Matrizes.length) {
+			debugger;
+			insertedMatrix();
+		}
+	}
+}
+
+class DisabledButton extends Button {
+	constructor(text, x, y, width, height) {
+		super(text, x, y, width, height)
+	}
+
+	draw() {
+		stroke(0, 0, 0);
+		// fill(100, 100, 100);
+		rect(this.x, this.y, this.width, this.height);
+		// fill(255, 255, 255);
+		textSize(15);
+		text(this.text, this.x + 5, this.y + 20);
+		// fill(0, 0, 0);
+	}
+}
+
+let buttons = [
+	new PushMatrixButton(455, 320, 112, 25),
+	new PopMatrixButton(455, 350, 105, 25),
+	new DisabledButton('glTranslate(...)', 35, 220, 110, 25),
+	new DisabledButton('glScale(...)', 35, 250, 90, 25),
+	new DisabledButton('glRotate(...)', 35, 280, 90, 25),
+	new LoadMatrixButton(35, 320, 120, 25),
+	new DisabledButton('glMultMatrix(...)', 35, 350, 115, 25)
+];
 
 function brackets(x, y) {
 	noStroke();
@@ -73,23 +183,24 @@ function Matrix(num, bottomLimit) {
 
 function ButtonPush() {
 	stroke(0, 0, 0);
-	rect(35, 350, 50, 25);
+	rect(35, 350, 112, 25);
 	textSize(15);
-	text("Push", 41, 367);
+	text("glPushMatrix( )", 41, 367);
 }
 
 function ButtonPop() {
 	stroke(0, 0, 0);
-	rect(515, 350, 50, 25);
+	rect(455, 350, 105, 25);
 	textSize(15);
-	text("Pop", 525, 367);
+	text("glPopMatrix( )", 461, 367);
 }
 
 function ButtonChangeMatrix() {
 	stroke(0, 0, 0);
-	rect(515, 300, 75, 25);
+	rect(455, 350, 105, 25);
+	rect(455, 300, 120, 25);
 	textSize(15);
-	text("Change", 525, 320);
+	text("glLoadMatrix(...)", 461, 320);
 }
 
 function cloud(x, y) {
@@ -106,7 +217,6 @@ function cloud(x, y) {
 function mainScreen() {
 	background(255, 255, 233, 80);
 	background(0, 191, 255, 200);
-	text("Elements : " + Matrizes.length, 20, 20);
 	cloud(100, 180);
 	cloud(400, 100);
 	tam += mulTam;
@@ -117,9 +227,11 @@ function mainScreen() {
 	fill(100, 255, 100);
 	rect(0, 370, width, 50);
 	pop();
-	ButtonChangeMatrix();
-	ButtonPush();
-	ButtonPop();
+
+	for (button of buttons) {
+		button.draw();
+	}
+
 	if (Matrizes.length > 0) {
 		text("Using", Matrizes[Matrizes.length - 1].pos.x + 45, Matrizes[Matrizes.length - 1].pos.y);
 	}
@@ -146,34 +258,11 @@ function insertedMatrix(matrix) {
 }
 
 function mouseClicked() {
-	if (mouseX > 35 && mouseX < 85 && mouseY > 350 && mouseY < 375) {
-		if (Matrizes.length == 0) {
-			var d3 = [];
-			arrayCopy(id2, d3);
-			Matrizes.push(new Matrix(d3, height - 51));
-		} else if (Matrizes.length == 1) {
-			if (Matrizes[Matrizes.length - 1].bot) {
-				var d4 = [];
-				arrayCopy(Matrizes[Matrizes.length - 1].numbers, d4);
-				Matrizes.push(new Matrix(d4, ((Matrizes[Matrizes.length - 1]).pos.y) - 80));
-			}
-		} else if (Matrizes.length < 4) {
-			if (Matrizes[Matrizes.length - 1].bot) {
-				var d5 = [];
-				arrayCopy(Matrizes[Matrizes.length - 1].numbers, d5);
-				Matrizes.push(new Matrix(d5, ((Matrizes[Matrizes.length - 1]).pos.y) - 80));
-			}
+	for (button of buttons) {
+		if (button.isPointWithin(mouseX, mouseY)) {
+			button.execute();
 		}
 	}
-	if (mouseX > 515 && mouseX < 565 && mouseY > 350 && mouseY < 375) {
-		Matrizes.splice(Matrizes.length - 1, 1);
-	}
-	if (mouseX > 515 && mouseX < 590 && mouseY > 300 && mouseY < 325) {
-		if (Matrizes.length > 0) {
-			insertedMatrix(Matrizes[Matrizes.length - 1].numbers);
-		}
-	}
-
 }
 
 function setup() {
@@ -189,5 +278,4 @@ function setup() {
 
 function draw() {
 	mainScreen();
-	//console.log("X : " + mouseX + " Y : " + mouseY);
 }
