016ce9432d633d0ba15da5361d895aff3a1bb4c8
Flavio Coutinho <fegemo@gmail.com>
2018-12-28 21:45:48 -0200

    Removes unused window.js

--- window.js
+++
@@ -1,8 +0,0 @@
-// $(document).ready(function() {
-//   $(window).scroll(function() {
-//     $("#holder").stop().animate({
-//       "padding-top": ($(window).scrollTop()) - 80 + "px",
-//       "marginLeft": ($(window).scrollLeft()) + "px"
-//     }, "slow");
-//   });
-// });
